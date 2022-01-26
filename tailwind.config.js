733bc5ad14ad4f4777005f93f403343bfcb536b9
Zoli Bende <zoltan@octopus-digital.pro>
2022-01-26 15:46:49 +0200

    general settgings

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+const plugin = require('tailwindcss/plugin')
+
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
